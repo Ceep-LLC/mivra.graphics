2ee88183c56f1e28cf4da0a6a767bf000a2b3f21
Kazushi Isogami <kazushiisogami@KazushinoMacBook-Pro.local>
2025-10-17 18:34:00 +0900

    set og image

--- src/app/layout.js
+++ src/app/layout.js
@@ -65,11 +65,11 @@ export const metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/images/favicon.ico" },
+      { url: "/images/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/images/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
-    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+    apple: [{ url: "/images/apple-touch-icon.png", sizes: "180x180" }],
   },
   robots: {
     index: true,
